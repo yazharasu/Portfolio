f4e2d93306d5f3ac54937b09afd33309937faf7b
Yazharasu <yazh.bis@gmail.com>
2023-04-24 22:03:47 +0530

    Update aboutme.js

--- src/Profile Container/About me/aboutme.js
+++ src/Profile Container/About me/aboutme.js
@@ -17,12 +17,16 @@ function AboutMe() {
 
                 <div className='about-me-profile'>
                     <div className='about-me'>
-                        <p>
-                            I'm working as a Software Developer at Iamneo.ai Edutech Pvt. Ltd.
-                            I'm a STEM Post graduate professional with experience in engineering and academics.
-                            
-                            Highly motivated professional looking for opportunities in Organisation which offers 
-                            challenging environment to augment my technical and leadership skills.
+                        <p>I'm Yazharasu, a creative problem-solver with a diverse background in product design, teaching, and front-end web development. 
+                            With 2 years of experience in product design and development and 5 years in teaching, I've honed my ability to think critically, communicate effectively, and collaborate with others to bring ideas to life. 
+                            And in the past year, I've been leveraging my skills in HTML, CSS, TailwindCSS, Bootstrap, JavaScript, TypeScript, and React to build dynamic and user-friendly web applications.
+                        </p>
+
+                        <p>But my journey to front-end engineering wasn't a straight line. In fact, I first dipped my toes in front-end technologies while teaching for a year, and I fell in love with the creativity and problem-solving involved in building web applications. 
+                            I am currently working as a senior software engineer at Iamneo.ai Edutech Pvt Ltd.
+                        </p>
+
+                        <p>Whether I'm working on a new project or collaborating with a team, I bring a positive attitude, a thirst for learning, and a commitment to delivering high-quality work. I'm also familiar with tools like Angular, Next.js, node.js, and mongoDB.
                         </p>
                         <h3>Profile Highlights...</h3>
                         <ul className='skill-list'>
