840e308b410ca04900707054714f95d752101af3
Yazharasu <yazh.bis@gmail.com>
2023-04-24 22:50:26 +0530

    Update aboutme.js

--- src/Profile Container/About me/aboutme.js
+++ src/Profile Container/About me/aboutme.js
@@ -22,11 +22,11 @@ function AboutMe() {
                             And in the past year, I've been leveraging my skills in HTML, CSS, TailwindCSS, Bootstrap, JavaScript, TypeScript, and React to build dynamic and user-friendly web applications.
                         </p>
 
-                        <p>But my journey to front-end engineering wasn't a straight line. In fact, I first dipped my toes in front-end technologies while teaching for a year, and I fell in love with the creativity and problem-solving involved in building web applications. 
-                            I am currently working as a senior software engineer at Iamneo.ai Edutech Pvt Ltd.
+                        <p>But my journey to front-end engineering wasn't a straight line. In fact, I first dipped my toes in front-end technologies while teaching for a year, and I fell in love with the creativity and problem-solving involved in building web apps. 
+                            I am currently working as a software engineer @ Iamneo.ai Edutech Pvt Ltd.
                         </p>
 
-                        <p>Whether I'm working on a new project or collaborating with a team, I bring a positive attitude, a thirst for learning, and a commitment to delivering high-quality work. I'm also familiar with tools like Angular, Next.js, node.js, and mongoDB.
+                        <p>Whether I'm working on a new project or collaborating with a team, I bring a positive attitude, a thirst for learning, and a commitment to delivering high-quality work. I'm also familiar with tools like Angular, Next.js, node.js, & mongoDB.
                         </p>
                         <div className='home-buttons'>
                             <a href='#contact-cotainer'>   
